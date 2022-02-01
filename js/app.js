d78a31652e3d4dcf8cec795a4332913221987dbe
namansikri <77356560+namansikri@users.noreply.github.com>
2022-02-01 20:07:44 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
 
+
 items.forEach((item) => {
   item.addEventListener("click", () => {
     document.querySelector("li.active").classList.remove("active");
